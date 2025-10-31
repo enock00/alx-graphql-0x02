4fec1842840aa8b6d1f398eeaafc080e00fb5c52
Enock <enockzy54@gmail.com>
2025-10-31 07:48:15 +0300

    Duplicated project alx-graphql-0x01 to alx-graphql-0x02

---
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold">Rick and Morty App Setup Successful ✅</h1>
+    </div>
+  )
+}
